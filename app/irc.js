f1d4c3d40b0ab06cc364ad23fcc2719b30cdbf5a
gorhgorh <gorhgorh@lesnodebots.eu>
2017-06-09 14:30:53 +0200

    irc bot base

---
+++ app/irc.js
@@ -0,0 +1,24 @@
+var config = {
+	channels: ["#cibot"],
+	server: "irc.freenode.net",
+	botName: "cibot"
+}
+
+var irc = require("irc");
+
+// Create the bot name
+var bot = new irc.Client(config.server, config.botName, {
+	channels: config.channels
+});
+
+// Listen for any message, PM said user when he posts
+bot.addListener("message", function(from, to, text, message) {
+
+    var passed = message.match(/The build passed/g)
+	console.log(passed)
+    if (passed.lenght) {
+        console.log("build passed yay")
+    }
+//    bot.say(config.channels[0], "¿Que? " + from);
+
+});
